70c797edc52a14b35d111f729efac3ecfa07aaf5
divljiboy <ivan93.ns@hotmail.com>
2016-05-14 20:13:13 +0200

    Idemo na Kipar

---
+++ PINF.Data/Configurations/DnevnoStanjeRacunaConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using PINF.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PINF.Data.Configurations
+{
+    public class DnevnoStanjeRacunaConfiguration : ModelBaseConfiguration<DnevnoStanjeRacuna>
+    {
+
+        public DnevnoStanjeRacunaConfiguration()
+        {
+            Property(x => x.DatumPrometa).IsRequired();
+            Property( x => x.NovoStanje).IsRequired().HasPrecision(15,2);
+            Property( x => x.PrethodnoStanje).IsRequired().HasPrecision(15, 2);
+            Property( x => x.PrometNaTeret).IsRequired().HasPrecision(15, 2);
+            Property( x => x.PrometUKorist).IsRequired().HasPrecision(15, 2);
+        }
+    }
+}
---
+++ PINF.Data/Configurations/VrstePlacanjaConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PINF.Data.Configurations
+{
+    public class VrstePlacanjaConfiguration:ModelBaseConfiguration<Model.Model.VrstePlacanja>
+    {
+
+        public VrstePlacanjaConfiguration()
+        {
+            Property(x => x.NazivVrstePlacanja).IsRequired().HasMaxLength(120);
+
+        }
+    }
+}
