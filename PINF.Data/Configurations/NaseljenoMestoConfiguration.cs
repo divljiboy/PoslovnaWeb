1d8920967e16a1abf4769d07c9615501ae764858
divljiboy <ivan93.ns@hotmail.com>
2016-05-14 20:16:00 +0200

    h

---
+++ PINF.Data/Configurations/NaseljenoMestoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using PINF.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PINF.Data.Configurations
+{
+    public class NaseljenoMestoConfiguration : ModelBaseConfiguration<NaseljenoMesto>
+    {
+
+        public NaseljenoMestoConfiguration()
+        {
+            Property(x => x.Naziv).IsRequired().HasMaxLength(60);
+            Property(x => x.PttOznaka).IsRequired().HasMaxLength(12);
+        }
+        
+    }
+}
